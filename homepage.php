9d65f50f65ae619a332583e5d6840433f11b6bd7
Chloe Umble <chloe@thehappyhome.co>
2017-03-07 19:52:20 -0800

    what does this do

---
+++ homepage.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html class=no-js>
+
+<head>
+    <meta charset=utf-8>
+    <meta http-equiv=X-UA-Compatible content="IE=edge">
+    <title>Chloe Umble</title>
+    <meta name=title content="Chloe Umble's Online Portfolio">
+    <meta name=description content="Hi! I'm Chloe Umble. This is where I keep examples of my work or things I've made. Please contact me or relevant business opportunities">
+    <meta property=og:title content="ChloesPage">
+    <meta name=viewport content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+    <link rel=icon href="../img/cufinal.gif" />
+    <link rel=stylesheet href="css/homepage.css">
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+</head>
+
+<body class="home">
+    <?php include 'header.html';?>
+    <div class="row">
+        <div class="col-md-12">
+            <section class="row homeHero parallax verticalCenter">
+                <h1 id ="home">Hello, world.</h1>
+            </section>
+            <section div class="row">
+                <div class="col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1 col-xs-10 col xs-offset-1">
+                    <p class="homepagebio">
+                        Hello! I'm Chloe - a maker, an internet enthusiast, and tea drinker.
+                        <br>
+                        <br> I'm looking to combine my experience in technical fields with the chance to create opportunities, inspire confidence and creativity, follow curiosity, ask questions, and seek out solutions.
+                        <br>
+                        <br> I have both high levels of enthusiasm and professionalism, a passion for learning, and practical experience working with others in stressful environments. I welcome the creative challenge of solving issues and puzzles, and always look to react quickly in effective and dynamic ways.
+                    </p>
+                </div>
+            </section>
+        </div>
+    </div>
+</body>
+
+</html>
